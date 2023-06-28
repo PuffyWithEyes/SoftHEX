08f637de42fd0bd1058b913ea9574613cd8378e9
PuffyWithEyes <123123Danya>
2023-06-28 22:40:18 +0300

    Add work with files

--- src/files/read.rs
+++ src/files/read.rs
@@ -1,7 +1,8 @@
 use std::fs;
+use super::Path;
 
 
-pub fn read_file(path: String) -> Option<String> {
+pub fn read_file(path: &Path) -> Option<String> {
     return match fs::read_to_string(path.clone()) {
         Ok(data) => Some(data),
         Err(_) => None,
