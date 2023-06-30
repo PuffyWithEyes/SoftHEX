a44a09702878ec2b3f4993110db2fcc40e2628f8
puffy_with_eyes <klimov-danya.04@mail.ru>
2023-06-30 15:39:59 +0300

    Try to add reading parameters with startup and load previous files in buffer

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::{error::Error, io};
+use std::{error::Error, io, env, fs, path};
 use tui::{
     backend::CrosstermBackend,
     Terminal,
 };
-use files::File;
+use files::{File, Path, write::CONFIG_OPENED_FILES_PATH, read::read_file};
 
 
 const LINE_NUMBER: u16 = 1_u16;
@@ -26,26 +26,63 @@ pub struct App {
 }
 
 
-impl Default for App {
-    fn default() -> Self {
-        App {
-            opened_files: Vec::new(),
-            tabs_titles: Vec::new(),
-            tabs_indexes: 0_usize,
-        }
-    }
+impl App {
+	pub fn new() -> Self {
+		App {
+			opened_files: Vec::new(),
+			tabs_titles: Vec::new(),
+			tabs_indexes: 0_usize,
+		}
+	}
+
+	pub fn add_file(&mut self, path: &Path) {
+		let file = File::new(path);
+		
+		let file_name = path::Path::new(path);
+		let file_name = file_name.file_name().unwrap().to_str().unwrap();
+		
+		self.opened_files.insert(0, file);
+		self.tabs_titles.insert(0, file_name.to_string());
+	}
+}
+
+
+fn load_all_files_in_app_buffer() {
+	let paths = fs::read_dir(CONFIG_OPENED_FILES_PATH).unwrap();
+
+	for file in paths {
+		let data = read_file(&file.unwrap().path().to_str().unwrap().to_string()).unwrap();
+		todo!("Написать пробежку по файлу конфига и последующую загрузку его в буффер");
+	}
 }
 
 
 
 fn main() -> Result<(), Box<dyn Error>> {  // TODO: 2
+	let args: Vec<String> = env::args().collect();
+
+	let app = App::new();
+
+	for arg in args {
+		if arg.to_lowercase() == "--help" || arg.to_lowercase() == "-h" {
+			todo!("Сделать help")
+		} else {
+			let file_path = fs::metadata(arg);
+
+			if file_path.is_ok() {
+				
+			} else {
+				
+			}
+		}
+	}
+	
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    let app = App::default();
     let res = run_app(&mut terminal, app);
 
     disable_raw_mode()?;
