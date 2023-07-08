a5128ba6e0a834700c250fdd9655fcfa3e6c5fb2
Puffy <klimov-danya.04@mail.ru>
2023-07-08 09:56:10 +0300

    Now that function return vector with string

--- src/files/read.rs
+++ src/files/read.rs
@@ -1,10 +1,17 @@
-use std::fs;
+use std::{fs, io::{BufReader, BufRead}};
 use super::Path;
 
 
-pub fn read_file(path: &Path) -> Option<String> {
-    return match fs::read_to_string(path.clone()) {
-        Ok(data) => Some(data),
-        Err(_) => None,
-    }
+pub fn read_file(path: &Path) -> Vec<String> {
+    let file = fs::File::open(path).unwrap();
+	let reader = BufReader::new(file);
+
+	let mut data_vec: Vec<String> = Vec::new();
+
+	for line in reader.lines() {
+		data_vec.push(line.unwrap())
+	}
+
+	data_vec
 }
+
