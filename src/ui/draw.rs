f1d5e9947137bae233ad26c7902e7069027c8534
Puffy <klimov-danya.04@mail.ru>
2023-07-13 17:07:06 +0300

    Add tabs and fix bugs

--- src/ui/draw.rs
+++ src/ui/draw.rs
@@ -3,9 +3,9 @@ use super::get_file_from_vec;
 use tui::{
     backend::Backend,
     layout::{Constraint, Direction, Layout, Alignment},
-    style::{Modifier, Style},
-    text::Span,
-    widgets::{Block, Borders, Paragraph, Wrap, BorderType},
+    style::{Modifier, Style, Color},
+    text::{Span, Spans},
+    widgets::{Tabs, Block, Borders, Paragraph, Wrap, BorderType},
     Frame,
 };
 
@@ -24,38 +24,61 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
 
 	match get_file_from_vec(app).file_mode {
 		FileState::Normal => {
-			let chunks = Layout::default()
+			let main_chunks = Layout::default()
+				.direction(Direction::Vertical)
+				.margin(2)
+				.constraints([
+					Constraint::Length(3), Constraint::Min(0)
+				].as_ref())
+				.split(f.size());
+			
+			let into_chunks = Layout::default()
                 .direction(Direction::Horizontal)
-                .margin(2)
                 .constraints([
                     Constraint::Percentage(65),
                     Constraint::Percentage(35)
                 ].as_ref())
-                .split(f.size());
+                .split(main_chunks[1]);
 
-            let test_file = File::new(
-                &"/home/puffy/dev/SoftHEX/test/test.txt".to_string()
-            );
-            app.opened_files.push(test_file);
+			let titles = app
+				.tabs_titles
+				.iter()
+				.map(|t| {
+					Spans::from(vec![
+						Span::styled(t, Style::default().fg(Color::Yellow)),
+					])
+				})
+				.collect();
+			
+			let tabs = Tabs::new(titles)
+				.block(Block::default().borders(Borders::ALL).title("Tabs"))
+				.select(app.tabs_indexes)
+				.highlight_style(
+					Style::default()
+						.add_modifier(Modifier::BOLD)
+						.bg(Color::Black),
+				);
+			f.render_widget(tabs, main_chunks[0]);
 
-            let test_file = app.opened_files.get(0).unwrap();
-            let text: Vec<String> = test_file.data.clone()
-				;
-			let text: String = text.into_iter().collect();
+			let file = get_file_from_vec(app);
+			let text: String = file.data.clone().into_iter().collect();
+            // let test_file = app.opened_files.get(0).unwrap();
+            // let text: Vec<String> = test_file.data.clone();
+			// let text: String = text.into_iter().collect();
 
             let paragraph = Paragraph::new(text.clone())
                 .block(create_block("HEX", Alignment::Center))
                 .alignment(Alignment::Left)
                 .wrap(Wrap { trim: true })
-                .scroll((test_file.scroll, 0));
-            f.render_widget(paragraph, chunks[0]);
+                .scroll((file.scroll, 0));
+            f.render_widget(paragraph, into_chunks[0]);
 
             let paragraph = Paragraph::new(text.clone())
-                .block(create_block("TEXT", Alignment::Center))
+                .block(create_block("Text", Alignment::Center))
                 .alignment(Alignment::Left)
                 .wrap(Wrap { trim: true })
-                .scroll((test_file.scroll, 0));
-            f.render_widget(paragraph, chunks[1]);
+                .scroll((file.scroll, 0));
+            f.render_widget(paragraph, into_chunks[1]);
 		},
 		_ => {},                                 
 	}
