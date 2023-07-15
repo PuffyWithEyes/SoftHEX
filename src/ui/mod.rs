81be253dded52575caca750cf15f1377c413b3e8
Puffy <klimov-danya.04@mail.ru>
2023-07-15 19:09:07 +0300

    I tried to make function for close tabs, but i can't :( But I refactore some code

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,32 +1,32 @@
 mod draw;
 
-use crate::{App, files::{FileState, File, write::make_or_save_config}};
+use crate::{
+	App,
+	files::{
+		FileState, Paths,
+		write::{PATH_IN_CONFIG_AT_VEC, make_or_save_config},
+		move_file::move_file,
+		read::read_file,
+	},
+};
 use draw::ui;
 use crossterm::event::{self, Event, KeyCode};
 use tui::{
     backend::Backend,
     Terminal,
 };
-use std::io;
-
-
-pub fn get_file_from_vec(app: &mut App) -> File {
-	let index_opened_tab = app.tabs_indexes;
-
-	app.opened_files.get(index_opened_tab).unwrap().clone()
-}
+use std::{io, fs};
 
 
 pub fn run_app<B: Backend>(
     terminal: &mut Terminal<B>,
     mut app: App,
 ) -> io::Result<()> {
-	
     loop {
         terminal.draw(|f| ui(f, &mut app))?;
 
         if let Event::Key(key) = event::read()? {
-			match get_file_from_vec(&mut app).file_mode {
+			match app.get_current_file().file_mode {
 				FileState::Normal => {
 					match key.code {
 						KeyCode::Char('d') | KeyCode::Char('D') | KeyCode::Char('в') | KeyCode::Char('В') |
@@ -39,35 +39,54 @@ pub fn run_app<B: Backend>(
 						},
 						KeyCode::Char('w') | KeyCode::Char('W') | KeyCode::Char('ц') | KeyCode::Char('Ц') |
 						KeyCode::Up => {
-							let mut file = get_file_from_vec(&mut app);
+							let file = app.get_current_file_mut();
 
 							file.page_up();
 						},
 						KeyCode::Char('s') | KeyCode::Char('S') | KeyCode::Char('ы') | KeyCode::Char('Ы') |
 						KeyCode::Down => {
-							let mut file = get_file_from_vec(&mut app);
+							let file = app.get_current_file_mut();
 
 							file.page_down();
 						},
 						KeyCode::Char('t') | KeyCode::Char('T') | KeyCode::Char('е') | KeyCode::Char('Е') => {
-							let mut file = get_file_from_vec(&mut app);
+							let file = app.get_current_file_mut();
 
 							file.file_mode = FileState::EditingText;
 						},
 						KeyCode::Char('h') | KeyCode::Char('H') | KeyCode::Char('р') | KeyCode::Char('Р') => {
-							let mut file = get_file_from_vec(&mut app);
+							let file = app.get_current_file_mut();
 
 							file.file_mode = FileState::EditingHex;
 						},
 						KeyCode::F(5) => {  
-							let mut file = get_file_from_vec(&mut app);
+							let file = app.get_current_file_mut();
 							
 							make_or_save_config(&file);
 
 							file.file_mode = FileState::Saved;
 						},
 						KeyCode::Char('c') | KeyCode::Char('C') | KeyCode::Char('с') | KeyCode::Char('С') => {
-							todo!("Сделать закрытие одноного из файлов в приложении")
+							if app.opened_files.len() == 1 {  // TODO: 11
+								return Ok(());
+							} else {
+								let default_paths = Paths::default();
+								let opened_files = fs::read_dir(default_paths.config_opened_files_path).unwrap();
+
+								for file_path in opened_files {
+									let config_file_data = read_file(
+										&file_path.unwrap().path().to_str().unwrap().to_string()
+									);
+
+									let path_of_file_in_conf = config_file_data.get(PATH_IN_CONFIG_AT_VEC);
+
+									//if path_of_file_in_conf == 
+								}
+								
+								//move_file(from, to);
+								
+								app.close_current_tab();
+							}
 						},
 						KeyCode::Char('q') | KeyCode::Char('Q') | KeyCode::Char('й') | KeyCode::Char('Й') => {
 							return Ok(());  // TODO: 11
