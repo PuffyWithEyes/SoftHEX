f0265b0872b76552b361b986ea37eb5448cee14d
Puffy <klimov-danya.04@mail.ru>
2023-07-15 19:06:53 +0300

    Remove structure with default paths and add some constants (rfactoring code)

--- src/files/write.rs
+++ src/files/write.rs
@@ -1,44 +1,10 @@
-use std::{fs, env, io::Write, path};
-use super::{Path, File, read::read_file};
+use std::{fs, io::Write, path};
+use super::{Path, File, Paths, CONFIG_EXTENSION, read::read_file};
 use uid::Id as IdT;
 
 
-pub struct Paths {
-	pub config_path: String,
-	pub config_softhex_path: String,
-	pub config_file_path: String,
-	pub config_opened_files_path: String,
-}
-
-
-impl Default for Paths {
-	fn default() -> Self {
-		let mut home_dir = env::home_dir().unwrap().to_str().unwrap().to_string();
-		home_dir.push('/');
-		
-		let mut conf_path = String::from(home_dir.clone());
-		conf_path.push_str(".config/");
-
-		let mut conf_softhex_path = String::from(home_dir.clone());
-		conf_softhex_path.push_str(".config/softhex/");
-
-		let mut conf_file_path = String::from(home_dir.clone());
-		conf_file_path.push_str(".config/softhex/opened_files.conf");
-
-		let mut conf_opened_file = String::from(home_dir);
-		conf_opened_file.push_str(".config/softhex/opened_files/");
-
-		Paths {
-			config_path: conf_path,
-			config_softhex_path: conf_softhex_path,
-			config_file_path: conf_file_path,
-			config_opened_files_path: conf_opened_file,
-		}
-	}
-}
-
-
-const CONFIG_EXTENSION: &str = ".conf";
+pub const PATH_IN_CONFIG_AT_VEC: usize = 0_usize;
+pub const SCROLL_IN_CONFIG_AT_VEC: usize = 1_usize;
 
 
 struct T(());
