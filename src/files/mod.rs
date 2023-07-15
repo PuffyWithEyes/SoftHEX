b0c97937c29f280b9c4b7f183f861658c4c8f87d
Puffy <klimov-danya.04@mail.ru>
2023-07-15 19:03:38 +0300

    Move structure with default paths from write.rs to here (refactor code)

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -1,8 +1,48 @@
+#![allow(deprecated)]
 pub mod read;
 pub mod write;
 pub mod move_file;
 
 use read::read_file;
+use std::env;
+
+
+pub struct Paths {
+	pub config_path: String,
+	pub config_softhex_path: String,
+	pub config_file_path: String,
+	pub config_opened_files_path: String,
+}
+
+
+impl Default for Paths {
+	fn default() -> Self {
+		let mut home_dir = env::home_dir().unwrap().to_str().unwrap().to_string();
+		home_dir.push('/');
+		
+		let mut conf_path = String::from(home_dir.clone());
+		conf_path.push_str(".config/");
+
+		let mut conf_softhex_path = String::from(home_dir.clone());
+		conf_softhex_path.push_str(".config/softhex/");
+
+		let mut conf_file_path = String::from(home_dir.clone());
+		conf_file_path.push_str(".config/softhex/opened_files.conf");
+
+		let mut conf_opened_file = String::from(home_dir);
+		conf_opened_file.push_str(".config/softhex/opened_files/");
+
+		Paths {
+			config_path: conf_path,
+			config_softhex_path: conf_softhex_path,
+			config_file_path: conf_file_path,
+			config_opened_files_path: conf_opened_file,
+		}
+	}
+}
+
+
+const CONFIG_EXTENSION: &str = ".conf";
 
 
 pub type Path = String;
